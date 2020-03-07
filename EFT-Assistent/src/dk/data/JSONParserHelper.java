aba90202cb093e260daa5d49e6fa90a685349e32
Mikkel Kuntz <flintfedt-trash@hotmail.com>
2020-03-07 12:37:49 +0100

    EFT: Cleanup of data folder and files.

--- EFT-Assistent/src/dk/data/JSONParserHelper.java
+++ EFT-Assistent/src/dk/data/JSONParserHelper.java
@@ -1,9 +1,12 @@
 package dk.data;
 
+import dk.model.MapType;
 import dk.model.PlayerInfo;
 import dk.model.SaveData;
 import dk.model.TraderType;
 import dk.model.quest.Quest;
+import dk.model.quest.QuestObjectives;
+import dk.model.tools.TypeParser;
 import org.json.*;
 import org.apache.commons.io.FileUtils;
 
@@ -13,6 +16,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+/** Has the responsibility for reading JSON files and parsing them into model object and vice versa.*/
 public class JSONParserHelper {
 
     private static final String dataFolderPath = "src\\dk\\data";
@@ -21,8 +25,78 @@ public class JSONParserHelper {
 
     private static final int numberOfSaveSlots = 2;
 
-    public JSONObject getAllQuests(){
-        return getJSONFromFile(questsDataPostfix);
+    public ArrayList<Quest> getAllQuestsFromFile(){
+        return parseAllQuests(getJSONFromFile(questsDataPostfix));
+    }
+
+    /** Takes in the topNode loaded from the saveData.json file
+     * and returns an array containing all loaded quests.*/
+    private ArrayList<Quest> parseAllQuests(JSONObject topNode){
+
+        JSONArray questsArrayJSON = topNode.getJSONArray("quests");
+
+        ArrayList<Quest> allParsedQuests = new ArrayList<>();
+
+        for(int i = 0; i < questsArrayJSON.length(); i++){
+            Quest parsedQuest = parseQuest(questsArrayJSON.getJSONObject(i));
+            allParsedQuests.add(parsedQuest);
+        }
+
+        return allParsedQuests;
+    }
+
+    private Quest parseQuest(JSONObject questObjectJSON){
+
+        String questName = (String) questObjectJSON.get("name");
+        String questGiver = (String) questObjectJSON.get("giver");
+        int questReqLevel = questObjectJSON.getInt("req_level");
+        ArrayList<String> questMaps = parseJSONArray(questObjectJSON.getJSONArray("maps"));
+        JSONArray objectivesJSONArray = questObjectJSON.getJSONArray("objectives");
+        ArrayList<String> questRequirements = parseJSONArray(questObjectJSON.getJSONArray("requirements"));
+
+        //Parsing from string to model objects
+        ArrayList<MapType> mapTypes = new ArrayList<>();
+        for(String questMap : questMaps)
+            mapTypes.add(TypeParser.stringToMapType(questMap));
+
+        TraderType trader = TypeParser.stringToTrader(questGiver);
+
+        ArrayList<QuestObjectives> questObjectivesStrings = parseObjectives(objectivesJSONArray);
+
+        boolean hasLoyaltyLevelReq = questObjectJSON.has("req_LL");
+        int reqLoyaltyLevel = 0;
+        if(hasLoyaltyLevelReq)
+            reqLoyaltyLevel = questObjectJSON.getInt("req_LL");
+
+        int id = questObjectJSON.getInt("id");
+
+        //Required quests
+        JSONArray reqQuestIdArrayJSON = questObjectJSON.getJSONArray("req_quests");
+        ArrayList<Integer> reqQuestId = parseJSONArrayToInt(reqQuestIdArrayJSON);
+
+        return new Quest(questName, mapTypes, trader, questReqLevel, questObjectivesStrings, questRequirements, reqLoyaltyLevel, id, reqQuestId);
+    }
+
+    private ArrayList<QuestObjectives> parseObjectives(JSONArray questObjectivesJSON){
+
+        ArrayList<QuestObjectives> objectives = new ArrayList<>();
+        for(int i = 0; i < questObjectivesJSON.length(); i++){
+            QuestObjectives qo = new QuestObjectives();
+            String objective = ((JSONObject)questObjectivesJSON.get(i)).getString("obj");
+            qo.setObjective(objective);
+
+
+            boolean hasSubObj = ((JSONObject)questObjectivesJSON.get(i)).has("subs");
+            if(hasSubObj){
+                JSONArray subObjectivesJSONArray = ((JSONObject)questObjectivesJSON.get(i)).getJSONArray("subs");
+                ArrayList<String> subObjs = parseJSONArray(subObjectivesJSONArray);
+                qo.setSubObjectives(subObjs);
+            }
+
+            objectives.add(qo);
+        }
+
+        return objectives;
     }
 
     public boolean SaveData(int saveSlotId, ArrayList<Quest> completedQuests, PlayerInfo playerInfo){
@@ -134,5 +208,19 @@ public class JSONParserHelper {
         }
 
         return stringArray;
+    }
+
+    private ArrayList<Integer> parseJSONArrayToInt(JSONArray JArray){
+        ArrayList<Integer> intArray = new ArrayList<>();
+        for(int i = 0; i < JArray.length(); i++){
+            int parsedInt = JArray.getInt(i);
+            intArray.add(parsedInt);
+        }
+
+        return intArray;
+    }
+
+    private QuestObjectives stringArrayToQObjectives(ArrayList<String> objectivesStringArray){
+        return new QuestObjectives(objectivesStringArray.get(0), null);
     }
 }
--- EFT-Assistent/src/dk/model/quest/QuestLoader.java
+++ EFT-Assistent/src/dk/model/quest/QuestLoader.java
@@ -13,9 +13,6 @@ import java.util.ArrayList;
 
 public class QuestLoader {
 
-    private static String json_topNodeId = "quests";
-
-
     public ArrayList<Quest> allQuest;
 
     public QuestLoader() {
@@ -23,101 +20,7 @@ public class QuestLoader {
     }
 
     private void loadAllQuests(){
-        allQuest = getAllQuestsFromFile();
-    }
-
-    private ArrayList<Quest> getAllQuestsFromFile(){
-        JSONParserHelper JPH = new JSONParserHelper();
-        JSONObject topObjectJSON = JPH.getAllQuests();
-
-        JSONArray questsArrayJSON = topObjectJSON.getJSONArray(json_topNodeId);
-
-        ArrayList<Quest> allParsedQuests = new ArrayList<>();
-
-        for(int i = 0; i < questsArrayJSON.length(); i++){
-            Quest parsedQuest = parseQuest(questsArrayJSON.getJSONObject(i));
-            allParsedQuests.add(parsedQuest);
-        }
-
-        return allParsedQuests;
-    }
-
-    private Quest parseQuest(JSONObject questObjectJSON){
-
-        String questName = (String) questObjectJSON.get("name");
-        String questGiver = (String) questObjectJSON.get("giver");
-        int questReqLevel = questObjectJSON.getInt("req_level");
-        ArrayList<String> questMaps = parseJSONArray(questObjectJSON.getJSONArray("maps"));
-        //JSONObject objectivesJSONObject = questObjectJSON.getJSONObject("objectives");
-        JSONArray objectivesJSONArray = questObjectJSON.getJSONArray("objectives");
-        ArrayList<String> questRequirements = parseJSONArray(questObjectJSON.getJSONArray("requirements"));
-
-        //Parsing from string to model objects
-        ArrayList<MapType> mapTypes = new ArrayList<>();
-        for(String questMap : questMaps)
-            mapTypes.add(TypeParser.stringToMapType(questMap));
-
-        TraderType trader = TypeParser.stringToTrader(questGiver);
-
-        ArrayList<QuestObjectives> questObjectivesStrings = parseObjectives(objectivesJSONArray);
-
-        boolean hasLoyaltyLevelReq = questObjectJSON.has("req_LL");
-        int reqLoyaltyLevel = 0;
-        if(hasLoyaltyLevelReq)
-            reqLoyaltyLevel = questObjectJSON.getInt("req_LL");
-
-        int id = questObjectJSON.getInt("id");
-
-        //Required quests
-        JSONArray reqQuestIdArrayJSON = questObjectJSON.getJSONArray("req_quests");
-        ArrayList<Integer> reqQuestId = parseJSONArrayToInt(reqQuestIdArrayJSON);
-
-        return new Quest(questName, mapTypes, trader, questReqLevel, questObjectivesStrings, questRequirements, reqLoyaltyLevel, id, reqQuestId);
-    }
-
-    private ArrayList<QuestObjectives> parseObjectives(JSONArray questObjectivesJSON){
-
-        ArrayList<QuestObjectives> objectives = new ArrayList<>();
-        for(int i = 0; i < questObjectivesJSON.length(); i++){
-            QuestObjectives qo = new QuestObjectives();
-            String objective = ((JSONObject)questObjectivesJSON.get(i)).getString("obj");
-            qo.setObjective(objective);
-
-
-            boolean hasSubObj = ((JSONObject)questObjectivesJSON.get(i)).has("subs");
-            if(hasSubObj){
-                JSONArray subObjectivesJSONArray = ((JSONObject)questObjectivesJSON.get(i)).getJSONArray("subs");
-                ArrayList<String> subObjs = parseJSONArray(subObjectivesJSONArray);
-                qo.setSubObjectives(subObjs);
-            }
-
-            objectives.add(qo);
-        }
-
-        return objectives;
-    }
-
-    private ArrayList<String> parseJSONArray(JSONArray JArray){
-        ArrayList<String> stringArray = new ArrayList<>();
-        for(int i = 0; i < JArray.length(); i++){
-            String parsedString = (String) JArray.get(i);
-            stringArray.add(parsedString);
-        }
-
-        return stringArray;
-    }
-
-    private ArrayList<Integer> parseJSONArrayToInt(JSONArray JArray){
-        ArrayList<Integer> intArray = new ArrayList<>();
-        for(int i = 0; i < JArray.length(); i++){
-            int parsedInt = JArray.getInt(i);
-            intArray.add(parsedInt);
-        }
-
-        return intArray;
-    }
-
-    private QuestObjectives stringArrayToQObjectives(ArrayList<String> objectivesStringArray){
-        return new QuestObjectives(objectivesStringArray.get(0), null);
+        JSONParserHelper jph = new JSONParserHelper();
+        allQuest = jph.getAllQuestsFromFile();
     }
 }
